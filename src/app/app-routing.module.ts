2feaaa5d0dd23e1fcc5c5c767557666ad7083989
Mychell Motta <mychellmt@gmail.com>
2022-12-05 23:21:06 -0300

    gitflow-feature-stash: feature-atividade-09

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,12 @@
+import { NewEntryComponent } from './new-entry/new-entry.component';
+import { LandPageComponent } from './land-page/land-page.component';
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+const routes: Routes = [{path: '', component: LandPageComponent},
+{path: 'new', component: NewEntryComponent},
+{path: 'new/:param', component: NewEntryComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/footer/footer.component.ts
+++ src/app/footer/footer.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
 
 @Component({
   selector: 'app-footer',
@@ -6,10 +6,10 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./footer.component.css'],
 })
 export class FooterComponent implements OnInit {
-  year: number;
+  @Input() year?: number;
   github: string;
   constructor() {
-    this.year = new Date().getFullYear();
+    // this.year = new Date().getFullYear();
     this.github = "https://github.com/mychellmotta/historico-de-precos";
   }
 
