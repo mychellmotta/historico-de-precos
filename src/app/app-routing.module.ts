48a51168bd3b844f75f6b6193119953c1cb984b5
Mychell Motta <mychellmt@gmail.com>
2022-12-09 00:58:48 -0300

    Correção de arquivos deletados

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,6 +1,7 @@
 import { NewEntryComponent } from './new-entry/new-entry.component';
 import { LandPageComponent } from './land-page/land-page.component';
 import { ProductComponent } from './product/product.component';
+import { StoreComponent } from './store/store.component';
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
